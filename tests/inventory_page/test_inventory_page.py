b10403d995025dd3f3a337eed5ce1b0ee3163098
Caio Gava <caio.gava@trustly.com>
2024-07-12 10:11:42 -0300

    Changing tests to classes

--- tests/inventory_page/test_inventory_page.py
+++ tests/inventory_page/test_inventory_page.py
@@ -4,31 +4,29 @@ from pages.inventory.InventoryPage import InventoryPage
 from pages.login.LoginPage import LoginPage
 
 
-@pytest.fixture(autouse=True, scope="function")
-def set_user_session(driver):
-    driver.get(LoginPage.URL)
-    driver.add_cookie({"name": "session-username", "value": "standard_user"})
+class TestInventoryPage:
+    @pytest.fixture(autouse=True, scope="function")
+    def set_user_session(self, driver):
+        driver.get(LoginPage.URL)
+        driver.add_cookie({"name": "session-username", "value": "standard_user"})
 
+    @pytest.fixture(scope="function")
+    def inventory_page(self, driver):
+        return InventoryPage(driver)
 
-@pytest.fixture(scope="function")
-def inventory_page(driver):
-    return InventoryPage(driver)
+    def test_inventory_page_should_list_all_products(self, inventory_page):
+        inventory_page.navigate()
+        product_names = inventory_page.get_all_product_names()
 
+        assert len(product_names) > 0
 
-def test_inventory_page_should_list_all_products(inventory_page):
-    inventory_page.navigate()
-    product_names = inventory_page.get_all_product_names()
+    def test_inventory_page_should_list_all_products_fully(self, inventory_page):
+        inventory_page.navigate()
+        products = inventory_page.get_all_products()
 
-    assert len(product_names) > 0
+        assert len(products) > 0, "At least one product should be listed on screen"
 
-
-def test_inventory_page_should_list_all_products_fully(inventory_page):
-    inventory_page.navigate()
-    products = inventory_page.get_all_products()
-
-    assert len(products) > 0, "At least one product should be listed on screen"
-
-    for product in products:
-        assert product.get_price(), "Price should not be empty"
-        assert product.get_product_name(), "Product name should not be empty"
-        assert product.get_product_description(), "Product description should not be empty"
+        for product in products:
+            assert product.get_price(), "Price should not be empty"
+            assert product.get_product_name(), "Product name should not be empty"
+            assert product.get_product_description(), "Product description should not be empty"
--- tests/login_page/test_login_page.py
+++ tests/login_page/test_login_page.py
@@ -4,71 +4,67 @@ from typing import Tuple
 import pytest
 
 
-@pytest.fixture(autouse=True, scope="function")
-def login_page(driver):
-    return LoginPage(driver)
+class TestLoginPage:
+    @pytest.fixture(autouse=True, scope="function")
+    def login_page(self, driver):
+        return LoginPage(driver)
 
+    def _get_credentials_from_page(self, login_page: LoginPage) -> Tuple:
+        accepted_usernames = login_page.get_accepted_usernames()
+        password = login_page.get_password()
 
-def _get_credentials_from_page(login_page: LoginPage) -> Tuple:
-    accepted_usernames = login_page.get_accepted_usernames()
-    password = login_page.get_password()
+        assert len(accepted_usernames) > 0, "the user's list should not be empty"
+        assert password, "the password should not be empty"
 
-    assert len(accepted_usernames) > 0, "the user's list should not be empty"
-    assert password, "the password should not be empty"
+        return accepted_usernames, password
 
-    return accepted_usernames, password
+    def test_valid_user_should_be_able_to_log_in(self, driver, login_page: LoginPage):
+        login_page.navigate()
+        (accepted_usernames, password) = self._get_credentials_from_page(login_page)
 
+        assert accepted_usernames[0] == "standard_user", "user should be 'standard_user'"
 
-def test_valid_user_should_be_able_to_log_in(driver, login_page: LoginPage):
-    login_page.navigate()
-    (accepted_usernames, password) = _get_credentials_from_page(login_page)
+        login_page.fill_in_username(accepted_usernames[0])
+        login_page.fill_in_password(password)
+        login_page.click_on_login_button()
 
-    assert accepted_usernames[0] == "standard_user", "user should be 'standard_user'"
+        login_page.wait_until_url_to_be(InventoryPage.URL)
 
-    login_page.fill_in_username(accepted_usernames[0])
-    login_page.fill_in_password(password)
-    login_page.click_on_login_button()
+        assert driver.title == "Swag Labs"
 
-    login_page.wait_until_url_to_be(InventoryPage.URL)
+    _data_provider_invalid_users = [
+        ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
+        ("unknown_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"),
+    ]
 
-    assert driver.title == "Swag Labs"
+    @pytest.mark.parametrize("username, password, expected_message", _data_provider_invalid_users)
+    def test_invalid_users_should_not_be_able_to_log_in(self, driver, login_page: LoginPage, username: str,
+                                                        password: str,
+                                                        expected_message: str):
+        login_page.navigate()
 
+        login_page.fill_in_username(username)
+        login_page.fill_in_password(password)
+        login_page.click_on_login_button()
 
-_data_provider_invalid_users = [
-    ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
-    ("unknown_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"),
-]
+        assert login_page.is_error_message_displayed(), "error message should be displayed"
+        assert login_page.get_error_message() == expected_message
 
+    _data_provider_erratic_users = [
+        "problem_user",
+        "performance_glitch_user",
+        "error_user",
+        "visual_user",
+    ]
 
-@pytest.mark.parametrize("username, password, expected_message", _data_provider_invalid_users)
-def test_invalid_users_should_not_be_able_to_log_in(driver, login_page: LoginPage, username: str, password: str,
-                                                    expected_message: str):
-    login_page.navigate()
+    @pytest.mark.parametrize("username", _data_provider_erratic_users)
+    def test_erratic_users_should_be_able_to_log_in(self, driver, login_page: LoginPage, username: str):
+        login_page.navigate()
 
-    login_page.fill_in_username(username)
-    login_page.fill_in_password(password)
-    login_page.click_on_login_button()
+        login_page.fill_in_username(username)
+        login_page.fill_in_password("secret_sauce")
+        login_page.click_on_login_button()
 
-    assert login_page.is_error_message_displayed(), "error message should be displayed"
-    assert login_page.get_error_message() == expected_message
+        login_page.wait_until_url_to_be(InventoryPage.URL)
 
-
-_data_provider_erratic_users = [
-    "problem_user",
-    "performance_glitch_user",
-    "error_user",
-    "visual_user",
-]
-
-
-@pytest.mark.parametrize("username", _data_provider_erratic_users)
-def test_erratic_users_should_be_able_to_log_in(driver, login_page: LoginPage, username: str):
-    login_page.navigate()
-
-    login_page.fill_in_username(username)
-    login_page.fill_in_password("secret_sauce")
-    login_page.click_on_login_button()
-
-    login_page.wait_until_url_to_be(InventoryPage.URL)
-
-    assert driver.title == "Swag Labs"
+        assert driver.title == "Swag Labs"
